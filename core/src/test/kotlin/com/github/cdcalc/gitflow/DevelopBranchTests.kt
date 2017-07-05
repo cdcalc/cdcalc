a2187dcf7762d71476124995037053e7ba1fbc8d
Örjan Sjöholm <orjansjoholm@gmail.com>
2017-07-05 12:47:23 +0200

    Added more complex test cases for hotfix and releases

--- core/src/test/kotlin/com/github/cdcalc/gitflow/DevelopBranchTests.kt
+++ core/src/test/kotlin/com/github/cdcalc/gitflow/DevelopBranchTests.kt
@@ -96,4 +96,55 @@ class DevelopBranchTests {
         assertEquals("1.3.0-beta.1", result.toString())
     }
 
+    @Test
+    fun should_handle_release() {
+        git.createTaggedCommit("v1.2.3-rc.0")
+        git.checkout("release/1.2.3", true).createCommit()
+        git.checkout("develop").createCommit()
+
+        git.checkout("develop").merge("release/1.2.3")
+        git.checkout("master").merge("release/1.2.3")
+        git.tag().let {
+            it.name = "v1.2.3"
+            it.message = "v1.2.3"
+            it.call()
+        }
+
+        git.checkout("develop")
+        val result: SemVer = sut.gitFacts().semVer
+        assertEquals("1.3.0-beta.1", result.toString())
+    }
+
+    @Test
+    fun should_handle_hotfix() {
+        git.createTaggedCommit("v1.2.3-rc.0")
+        git.checkout("release/1.2.3", true).createCommit()
+        git.checkout("develop").createCommit()
+
+        git.checkout("develop").merge("release/1.2.3")
+        git.checkout("master").merge("release/1.2.3")
+        git.tag().let {
+            it.name = "v1.2.3"
+            it.message = "v1.2.3"
+            it.call()
+        }
+
+        git.checkout("hotfix/1.2.4", true)
+        git.createCommit().createCommit()
+
+        git.checkout("master").merge("hotfix/1.2.4")
+        git.tag().let {
+            it.name = "v1.2.4"
+            it.message = "v1.2.4"
+            it.call()
+        }
+
+        git.checkout("develop").merge("hotfix/1.2.4")
+
+        git.prettyLog()
+
+        val result: SemVer = sut.gitFacts().semVer
+        assertEquals("1.3.0-beta.2", result.toString())
+    }
+
 }
