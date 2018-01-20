1821c7907538379e30a6cdf579a3aa3e7a8f1ef5
Örjan Sjöholm <orjansjoholm@gmail.com>
2018-01-20 13:07:47 +0100

    Added extra properties to the calculateVersion task
    Calculated properties had been added to the calcualteVersion task:
    - calculateVersion.version for the calculated version
    - calculateVersion.branch for the branch used for the calculation
    
    Deprecated every thing in the cdcalc extension exception repository:
    cdcalc {
      repository = '../.git'
    }

--- plugin/src/main/kotlin/com/github/cdcalc/gradle/CalculateVersionTask.kt
+++ plugin/src/main/kotlin/com/github/cdcalc/gradle/CalculateVersionTask.kt
@@ -2,28 +2,31 @@ package com.github.cdcalc.gradle
 
 import com.github.cdcalc.Calculate
 import com.github.cdcalc.CalculateSetting
+import com.github.cdcalc.GitFacts
 import org.eclipse.jgit.api.Git
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 open class CalculateVersionTask : DefaultTask() {
-    override fun getDescription(): String {
-        return "Will calculate the upcoming version tracking branches and tags"
+    init {
+        group = "Release management"
+        description = "Will calculate the upcoming version tracking branches and tags"
     }
 
-    override fun getGroup(): String {
-        return "Release management"
-    }
-
-    @Suppress("unused")
     @TaskAction fun calculateVersion() {
         val config: CDCalcExtensions = project.extensions.findByType(CDCalcExtensions::class.java) ?: CDCalcExtensions()
-
         val git = Git.open(File(config.repository))
         val gitFacts = Calculate(git, CalculateSetting(config.trackOrigin)).gitFacts()
 
         config.calculatedVersion = gitFacts.semVer.toString()
         config.branch = gitFacts.branch
+
+        addProperties(gitFacts)
+    }
+
+    private fun addProperties(facts: GitFacts) {
+        extensions.extraProperties["branch"] = facts.branch
+        extensions.extraProperties["version"] = facts.semVer.toString()
     }
 }
