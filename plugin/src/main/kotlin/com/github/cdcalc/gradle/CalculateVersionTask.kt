1548ee6f78b0b1bb31b59b01ce0ef7b8682c60f2
Örjan Sjöholm <orjansjoholm@gmail.com>
2018-12-25 23:48:37 +0100

    Initial take on setting the publishing version from gradle

--- plugin/src/main/kotlin/com/github/cdcalc/gradle/CalculateVersionTask.kt
+++ plugin/src/main/kotlin/com/github/cdcalc/gradle/CalculateVersionTask.kt
@@ -5,6 +5,7 @@ import com.github.cdcalc.CalculateSettings
 import com.github.cdcalc.GitFacts
 import org.eclipse.jgit.api.Git
 import org.gradle.api.DefaultTask
+import org.gradle.api.logging.LogLevel
 import org.gradle.api.tasks.TaskAction
 
 open class CalculateVersionTask : DefaultTask() {
@@ -14,12 +15,22 @@ open class CalculateVersionTask : DefaultTask() {
     }
 
     @TaskAction fun calculateVersion() {
+        logRuntimeVersion()
+
         val config: CDCalcExtensions = project.extensions.findByType(CDCalcExtensions::class.java) ?: CDCalcExtensions()
         val git = Git.open(config.repository)
         val gitFacts = Calculate(git, CalculateSettings(versionFile = config.versionFile))
                 .gitFacts(logger::lifecycle)
 
         addProperties(gitFacts)
+
+        project.version = gitFacts.semVer.toString()
+    }
+
+    private fun logRuntimeVersion() {
+        val corePackage = Calculate::class.java.getPackage()
+        val implementationVersion = corePackage.implementationVersion
+        this.logger.log(LogLevel.INFO, "Version of cdcalc: $implementationVersion")
     }
 
     private fun addProperties(facts: GitFacts) {
--- plugin/src/test/kotlin/com/github/cdcalc/gradle/CalculateVersionTaskFunctionalTest.kt
+++ plugin/src/test/kotlin/com/github/cdcalc/gradle/CalculateVersionTaskFunctionalTest.kt
@@ -23,6 +23,56 @@ class CalculateVersionTaskFunctionalTest {
     private lateinit var testProjectDir: Path
 
     @Test
+    fun `Should output version`() {
+        val buildFile = testProjectDir.newFile("build.gradle")
+
+        val content = """
+            plugins {
+              id "com.github.cdcalc"
+            }
+        """.trimIndent()
+        buildFile.writeText(content)
+
+        gitInit(testProjectDir.toFile())
+                .commit("Initial commit")
+                .tag("v1.2.3")
+
+        val result = runGradle(testProjectDir.toFile(), "calculateVersion")
+
+        val task = result.task(":calculateVersion")!!
+        assertEquals(task.outcome, SUCCESS)
+        assertContains("1.2.3", result.output)
+    }
+
+    @Test
+    fun `Should set the project version`() {
+        val buildFile = testProjectDir.newFile("build.gradle")
+
+        val content = """
+            plugins {
+              id "com.github.cdcalc"
+            }
+
+            task printVersion(dependsOn: 'calculateVersion') {
+                doLast {
+                    println "assertVersion[${'$'}project.version]"
+                }
+            }
+        """.trimIndent()
+        buildFile.writeText(content)
+
+        gitInit(testProjectDir.toFile())
+                .commit("Initial commit")
+                .tag("v1.2.3")
+
+        val result = runGradle(testProjectDir.toFile(), "printVersion")
+
+        val task = result.task(":printVersion")!!
+        assertEquals(task.outcome, SUCCESS)
+        assertContains("assertVersion[1.2.3]", result.output)
+    }
+
+    @Test
     fun `Should add an extra property for version at calculateVersion`() {
         val buildFile = testProjectDir.newFile("build.gradle")
 
@@ -143,6 +193,7 @@ class CalculateVersionTaskFunctionalTest {
         return GradleRunner.create()
                 .withPluginClasspath()
                 .withProjectDir(projectDir)
+                .forwardOutput()
                 .withArguments(defaultArguments.plus(arguments))
                 .withDebug(true)
                 .build()
