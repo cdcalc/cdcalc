404ab7d5c933609d24057985fb71a071677176de
Örjan Sjöholm <orjansjoholm@gmail.com>
2018-12-26 20:54:58 +0100

    Prepare for publish via travis

---
+++ build-via-travis.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SWITCHES="--info --stacktrace"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
+  ./gradlew check $SWITCHES
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+  ./gradlew check $SWITCHES
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  ./gradlew check $SWITCHES
+else
+  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+  ./gradlew check $SWITCHES
+fi
+
+EXIT=$?
+
+exit $EXIT
